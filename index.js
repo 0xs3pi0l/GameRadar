a871bff1bbd56ed97e1e09caef74ee7c8a467942
0xs3pi0l <omarpasculli2001@gmail.com>
2023-05-12 17:17:28 +0200

    Change second game category to highest rated games of the year

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ async function insertPopularGames(){
   let gameCoverUrl;
   
   for(let i = 0; i < 5; i++){
-    console.log(popularGamesCards[i].childNodes[3].childNodes[3]);
     gameCoverUrl = jsonData[i].cover.url.replace("t_thumb", "t_cover_big");
     gameCoverUrl = `https:${gameCoverUrl}`
     popularGamesCards[i].childNodes[1].src = gameCoverUrl;
@@ -29,6 +28,18 @@ async function insertPopularGames(){
     popularGamesCards[i].childNodes[3].childNodes[3].innerText = jsonData[i].genres[0].name;
   }
 
+}
+
+async function insertHighestRatedGames(){
+  let highestRatedGamesCards = document.getElementsByClassName("highest-rated-container__cards-container__card");
+  let query = "fields name,release_dates.date,follows,genres.name,cover.url,total_rating; where release_dates.date > 1673084012; sort total_rating desc ; limit 6;";
+  let url = `http://localhost:8080/https://api.igdb.com:443/v4/games/`
+  let jsonData = await fetchData(url, query);
+  let gameCoverUrl;
+
+  for(let i = 0; i < 6; i++){
+
+  }
 }
 
 insertPopularGames();
